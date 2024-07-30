fee0208d03a665ae2bd804b9c6d297449eb15451
Beetroot Paul <paul@beetrootpaul.com>
2024-07-30 11:56:55 +0200

    validate project name

--- init-game.js
+++ init-game.js
@@ -2,18 +2,26 @@
 
 'use strict';
 
-console.log("WE ARE HERE!");
+const expectedNodeMinVersionMajor = 18;
+const currentNodeVersionMajor = process.versions.node?.split('.')?.[0];
 
-const currentNodeVersion = process.versions.node;
-const semver = currentNodeVersion.split('.');
-const major = semver[0];
-
-console.log(`Running in Node.js ${major}`);
-
-if (process.argv.length < 3) {
-    console.error("Project name was not provided");
+if (isNaN(Number(currentNodeVersionMajor)) || Number(currentNodeVersionMajor) < expectedNodeMinVersionMajor) {
+    console.warn(
+        `This tool is supposed to be run under Node.js ${expectedNodeMinVersionMajor} or newer. Currently run under Node.js version: ${process.version}.`
+    );
     process.exit(1);
 }
 
+if (process.argv.length < 3) {
+    console.warn("Project name has to be provided as a first argument.");
+    process.exit(1);
+}
 const projectName = process.argv[2];
+
+const projectNameConstraints = /^[-_0-9a-zA-Z]+$/;
+if (!projectNameConstraints.test(projectName)) {
+    console.warn(`Project name has to match a following RegExp: ${projectNameConstraints.toString()}`);
+    process.exit(1);
+}
+
 console.log(`Will create project: ${projectName}`);
