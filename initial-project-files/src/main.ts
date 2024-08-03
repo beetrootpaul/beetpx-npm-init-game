3e096a5f31e4daf74b2493e39844fb7a9adc6ed0
Beetroot Paul <paul@beetrootpaul.com>
2024-08-03 03:50:16 +0200

    update to BeetPx 0.51.0

--- initial-project-files/src/main.ts
+++ initial-project-files/src/main.ts
@@ -1,12 +1,53 @@
-import {$, $d, $rgb, $rgb_p8, $spr, $u, $v_0_0, $v_1_1} from "@beetpx/beetpx";
+import {$, $d, $rgb, $rgb_p8, $spr, $u, $v_0_0, $v_1_1, BpxVector2d} from "@beetpx/beetpx";
+
+const logoSprite = $spr("logo.png")(16, 16, 0, 0);
+let logoPosition: BpxVector2d;
 
 /**
- * `init` is used to configure BeetPx app and initialize underlying
- *  singleton used later to access the whole API (drawing functions etc.)
- *
- *  `$` is a shorter way of accessing `BeetPx`.
+ * `$` is a shorter way of accessing `BeetPx`.
  */
-$.init({
+$.setOnStarted(() => {
+    logoPosition = $.canvasSize.div(2);
+    $.startPlaybackLooped("music.flac");
+});
+
+/**
+ * `setOnUpdate` is used for a logic to be run in a fixed timestep
+ * game loop.
+ */
+$.setOnUpdate(() => {
+    /**
+     * `getPressedDirection` returns a 2D vector representing the pressed
+     * state of directional buttons. For example: "right" is (1,0).
+     */
+    const positionDiff = $.getPressedDirection().mul(3);
+    logoPosition = logoPosition.add(positionDiff).clamp($v_0_0, $.canvasSize);
+});
+
+/**
+ * `setOnDraw` is used for drawing, which might be an expensive operation
+ * (because of processing every pixel of the game canvas individually).
+ *
+ * The callback passed here is *not* guaranteed to be called on a fixed
+ * timestamp, in a contrary to the one passed to `setOnUpdated`.
+ */
+$.setOnDraw(() => {
+    /**
+     * `$d` is a shorter way of accessing `BeetPxDraw`.
+     */
+    $d.clearCanvas($rgb_p8.storm);
+    $d.sprite(logoSprite, logoPosition, {centerXy: [true, true]});
+    /**
+     * `$u` is a shorter way of accessing `BeetPxUtils`.
+     */
+    $u.drawTextWithOutline("BeetPx", $v_1_1, $rgb("#ff6e59"), $rgb("#125359"));
+});
+
+/**
+ * `start` is used to configure BeetPx app and start it.
+ */
+$.start({
+    gameId: "@beetpx/example-basic",
 
     /**
      * Here are names of asset files to be fetched by the BeetPx on load
@@ -16,47 +57,4 @@ $.init({
         "logo.png",
         "music.flac",
     ],
-
-}).then(async ({startGame}) => {
-    const logoSprite = $spr("logo.png")(16, 16, 0, 0);
-    let logoPosition = $.canvasSize.div(2);
-
-    $.startPlaybackLooped("music.flac");
-
-    /**
-     * `setOnUpdate` is used for a logic to be run in a fixed timestep
-     * game loop.
-     */
-    $.setOnUpdate(() => {
-        /**
-         * `getPressedDirection` returns a 2D vector representing the pressed
-         * state of directional buttons. For example: "right" is (1,0).
-         */
-        const positionDiff = $.getPressedDirection().mul(3);
-        logoPosition = logoPosition.add(positionDiff).clamp($v_0_0, $.canvasSize);
-    });
-
-    /**
-     * `setOnDraw` is used for drawing, which might be an expensive operation
-     * (because of processing every pixel of the game canvas individually).
-     *
-     * The callback passed here is *not* guaranteed to be called on a fixed
-     * timestamp, in a contrary to the one passed to `setOnUpdated`.
-     */
-    $.setOnDraw(() => {
-        /**
-         * `$d` is a shorter way of accessing `BeetPx.draw`.
-         */
-        $d.clearCanvas($rgb_p8.storm);
-        $d.sprite(logoSprite, logoPosition, {centerXy: [true, true]});
-        /**
-         * `$u` is a shorter way of accessing `BeetPx.draw`.
-         */
-        $u.drawTextWithOutline("BeetPx", $v_1_1, $rgb("#ff6e59"), $rgb("#125359"));
-    });
-
-    /**
-     * At the very end, let's not forget to start the game loop :)
-     */
-    await startGame();
 });
